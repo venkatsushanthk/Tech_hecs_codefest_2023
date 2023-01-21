6f60f99b538230e05200197ce764ee10d38783e8
venkatsushanthk <121592277+venkatsushanthk@users.noreply.github.com>
2023-01-21 11:04:50 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,62 @@
+# Codefest 2023.
+
+import time
+
+
+
+# The user is told about the game.
+print("Hello welcome to the game.\n"
+      "This is a text-adventure game.\n"
+      "It has different ending and stories so please play.\n\n")
+
+
+
+# The user is asked if he wants to play.
+while True:
+    ready = input("Are you ready? ").lower()
+    if ready == "yes":
+        break
+    else:
+        print("Take your time it's fine.\n")
+
+
+      
+# Game begins here
+# The story is told.
+print("\n\n")
+print("You wake up in a dark isolated room...\n")
+time.sleep(6) 
+print("It seems you are in a test tube of sorts.\n")
+time.sleep(6)
+print("You look around , all you see is People in black suits...\n")
+time.sleep(7) 
+print("...They seem to be talking to eachother but you can only make out a few words.\n")
+time.sleep(7) 
+print("You close your eyes... When you open them you are in an unknown place.\n")
+time.sleep(7)
+print("You Are no longer in the test tube .. But you are now in front of a unknown being...\n")
+time.sleep(8)
+print("You look around , It seems you are at the entrance of a dungeon.\n")
+time.sleep(7)
+
+# The user is told about the gates of hell
+print("Unknown being - Hello Human I am The Guardian of these gates.. I will let you pass without any trouble but... you will die in there , These are the gates to what you call hell... I suggest you tread with caution.\n")
+time.sleep(15)
+print("Guardian - You may wonder why would you want to go through hell... but its your only choice if you want to be reincarnated into a new body.\n")
+time.sleep(10)
+print("Guardian - I will present you a opportunity to survive in hell... because if you enter with only your mortal strength you will surely die...\n")
+time.sleep(10)
+print("Guardian - If you decide to trade all your memories before you reached here , I will present you with the opportunity To choose one of the 5 elemental powers to accompany you on your journey through the realm of hell.\n")
+time.sleep(20)
+print("Guardian - If you dont accept this offer you will most likely die in hell..\n\n")
+time.sleep(4)
+#Choice of powers.
+while True:
+  offer = input("Do you accept the offer? Type yes or no.\n").lower()
+  if offer == "yes":
+    print("Guardian - I will take your memories when you have chosen your power.\n")
+    break
+  if offer == "":
+    break
+
+
